ebeb951e7e033e955d3a7d5b37ad67ca9cbf7386
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-09-12 21:37:27 +0530

    cookies

--- app/views.py
+++ app/views.py
@@ -1,31 +1,34 @@
 from django.shortcuts import render
-
-# Create your views here.
 from datetime import *
 
 def set(request):
-    data =render(request,'set.html')
-    data.set_cookie('name','vaishu', max_age=2*24*60*60, httponly=True,secure=True)
-    data.set_cookie('age','22',max_age=4*24*60*60, httponly=True,secure=True)
-    data.set_cookie('city','Bhopal',max_age=3*24*60*60, httponly=True,secure=True)
+   
+    my_list1={'id':1,'name':'surya','city':'bhopal'}
+    my_list2={'id':2,'name':'ram','city':'indore'}
+    my_list= [my_list1,my_list2]
+    request.session['data']=my_list
+    return render(request,'set.html')
 
-    return data
+    # data.set_cookie('name','vaishu', max_age=2*24*60*60, httponly=True,secure=True)
+    # data.set_cookie('age','22',max_age=4*24*60*60, httponly=True,secure=True)
+    # data.set_cookie('city','Bhopal',max_age=3*24*60*60, httponly=True,secure=True)
+    # return data
 
 def get(request):
-    print(request.COOKIES)
-    nm = request.COOKIES['name']
-    ag = request.COOKIES['age']
-    ct = request.COOKIES['city']
 
-    data ={
+    data1= request.session.get('data','Guest')
+    return render(request, 'get.html',{'name':data1})
 
-        'name': nm,
-        'age': ag,
-        'city': ct,
-
-    }
-
-    return render(request,'get.html',data)
+    # print(request.COOKIES)
+    # nm = request.COOKIES['name']
+    # ag = request.COOKIES['age']
+    # ct = request.COOKIES['city']
+    # data ={
+    #     'name': nm,
+    #     'age': ag,
+    #     'city': ct,
+    # }
+    # return render(request,'get.html',data)
 
 def delete(request):
     data =  render(request,'delete.html')
